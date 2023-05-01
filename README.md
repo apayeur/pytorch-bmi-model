7fb39bf8d7fa5633b3c7217431a54a38cbfc220e
Alexandre Payeur <apaye026@gmail.com>
2023-05-01 15:54:58 -0400

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # pytorch-bmi-model
+Simulation of a BMI center-out reaching task using a recurrent neural network (RNN). The RNN can control either an arm model or a cursor.
+
--- test/test_point_mass_arm.py
+++ test/test_point_mass_arm.py
@@ -1,9 +1,10 @@
 from effectors import PointMassArm
 import torch
 
-pma = PointMassArm(workspace_dim=3)
+workspace_dim = 3
+pma = PointMassArm(workspace_dim=workspace_dim)
 
 # Produce some fake controls
-controls = torch.randn((8, 15, 3))
+controls = torch.randn((8, 15, workspace_dim))
 
 o = pma(controls)
